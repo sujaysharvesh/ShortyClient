ac1abd597e34ea2176bb3f4d2bae66281ce322a5
sharveshsujay <sujaysharvesh098@gmail.com>
2026-02-27 11:51:58 +0530

    update redirec page

--- app/[shortCode]/page.tsx
+++ app/[shortCode]/page.tsx
@@ -1,35 +1,25 @@
 import { notFound, redirect } from "next/navigation";
 
 type Props = {
-  params: {
-    shortCode: string;
-  };
+  params: Promise<{ shortCode: string }>;
 };
 
 export default async function ShortRedirect({ params }: Props) {
+
+  const { shortCode } = await params;
+
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/${params.shortCode}`,
+    `${process.env.NEXT_PUBLIC_API_URL}/${shortCode}`,
     {
       method: "GET",
-      redirect: "manual", 
+      redirect: "manual",
       cache: "no-store",
     }
   );
 
-  // Backend sends 302 redirect
   if (res.status === 302 || res.status === 301) {
     const location = res.headers.get("location");
-
-    if (location) {
-      redirect(location);
-    }
+    if (location) redirect(location);
   }
-
-  // If short code not found
-  if (res.status === 404) {
-    notFound();
-  }
-
-  // fallback
   notFound();
 }
--- next.config.ts
+++ next.config.ts
@@ -36,14 +36,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/:shortCode",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:shortCode`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
